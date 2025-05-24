c898030a94f5c21e11909141c4d6c34e2f8f7ca0
adrieldev <semmenor1@gmail.com>
2025-05-24 09:44:49 -0300

    Update Tarefacheck.js

--- Tarefacheck.js
+++ Tarefacheck.js
@@ -333,7 +333,7 @@
         await Promise.all([
             loadCss('https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap'),
         ]);
-        notifications.success(`TarefasResolver iniciado com sucesso!`, 3000);
+        notifications.success(`TarefasResolver iniciado com sucesso! feito por @Adrielzzz300`, 3000);
         notifications.info("Aguardando o login no Sala do Futuro...", 6000);
         enableSecurityMeasures();
     } catch (error) {
